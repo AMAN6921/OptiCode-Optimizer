bf7949f5b57a819b85d5f9ac7ed8abb12f091981
Aman Devrani <Aman.Devrani6921@gmail.com>
2026-04-23 22:04:38 +0530

    Delete owl-ai-gateway-main directory

--- owl-ai-gateway-main/src/components/home/index.ts
+++
@@ -1,7 +0,0 @@
-export { HeroSection } from "./HeroSection";
-export { FeaturesSection } from "./FeaturesSection";
-export { CoursesSection } from "./CoursesSection";
-export { TestimonialsSection } from "./TestimonialsSection";
-export { CTASection } from "./CTASection";
-export { FAQSection } from "./FAQSection";
-export { NewsletterSection } from "./NewsletterSection";
